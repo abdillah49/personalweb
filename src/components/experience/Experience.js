d9eb675bda60673c474b568d813c1e5a7f20cbf2
abdillah49 <m.abdillah48@gmail.com>
2020-10-02 14:56:53 +0700

    Update Experience.js

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -8,21 +8,21 @@ const Experience = () => {
                 <li>
                     <div class="timeline-user"><div></div></div>
                     <div class="timeline-content">
-                        <a>Des 2016 ~ Now</a><br />
+                        <span>Des 2016 ~ Now</span><br />
                         Clerk of HC System PT INALUM (Persero)
                         <div class="text">Experienced Clerk with a demonstrated history of working in the mining &amp; metals industry. Skilled in PHP, Jquery, MySQL, Bootstrap, HTML5 and CSS3.</div>
                     </div>
                 </li><li>
                     <div class="timeline-user"><div></div></div>
                     <div class="timeline-content">
-                        <a>Jul 2014 ~ Nov 2016</a><br />
+                        <span>Jul 2014 ~ Nov 2016</span><br />
                         Web Developer of Information Technology PT. Pastindo Sejahtera Bersama
                         <div class="text">Experienced Clerk with a demonstrated history of Information Technology. Skilled in PHP, Jquery, MySQL, Bootstrap, HTML5 and CSS3.</div>
                     </div>
                 </li><li>
                     <div class="timeline-user"><div></div></div>
                     <div class="timeline-content">
-                        <a>Sep 2012 ~ Jun 2014</a><br />
+                        <span>Sep 2012 ~ Jun 2014</span><br />
                         Web Developer of Information Technology MPS Soft
                         <div class="text">Experienced Clerk with a demonstrated history of Information Technology. Skilled in PHP, CodeIgniter, Jquery, MySQL, Bootstrap, HTML5 and CSS3.</div>
                     </div>
